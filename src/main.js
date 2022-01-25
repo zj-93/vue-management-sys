b6e25f1e923a190795fb8d4108c4dd2e6ca25c5a
zhoujun <15871906573>
2022-01-25 16:09:38 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import router from './router/index.js'
 import store from './store'
 // import '../node_modules/swiper/swiper.min.css';
